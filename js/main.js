3e9de257c4f094b0896ef6ecb90183fb53ca48e6
joe feng <itisjoe963@gmail.com>
2016-04-19 15:36:02 +0800

    update list

--- js/main.js
+++ js/main.js
@@ -21,6 +21,7 @@ $(function(){
         success: function(data) {
             for (var k in data) {
                 var d = data[k];
+                var url = '#';
                 if (k == 'z') {
                     continue;
                 }
@@ -29,12 +30,14 @@ $(function(){
                 c += '<div class="item-info">';
                 if (d.hasOwnProperty('homepage') && d.homepage != '') {
                     c += '<a href="'+d.homepage+'"><span aria-hidden="true" class="glyphicon glyphicon-home"></span></a> ';
+                    url = d.homepage;
                 }
                 if (d.hasOwnProperty('shop') && d.shop != '') {
                     c += '<a href="'+d.shop+'"><span aria-hidden="true" class="glyphicon glyphicon-shopping-cart"></span></a> ';
+                    url = d.shop;
                 }
                 c += '</div>';
-                c += '<div class="item-title"><a href="'+d.homepage+'">'+d.title+'</a></div>';
+                c += '<div class="item-title"><a href="'+url+'">'+d.title+'</a></div>';
                 c += '<div class="item-description">'+d.description+'</div>';
                 c += '<h3 class="item-tags"></h3></td></tr>';
 
